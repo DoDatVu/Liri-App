0d8069f7f0a3c105b2000b8eb636fab363f67c2a
DoDatVu <datvu22@gmail.com>
2019-03-27 16:38:14 -0500

    updated

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,24 +1,59 @@
-require("dotenv").config();
 
+require("dotenv").config();
+var command = process.argv[2];
+var Command = process.argv[3];
+var axios = require("axios");
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-switch (action) {
+
+
+switch (command) {
   case "concert-this":
-    total();
+    concertCommand();
     break;
-  
+
   case "dspotify-this-song":
-    deposit();
+    spotifyCommand();
     break;
-  
+
   case "movie-this":
-    withdraw();
+    movieCommand();
     break;
-  
+
   case "do-what-it-says":
-    lotto();
+    sayCommand();
     break;
-  }
-  
-  
+}
+
+function concertCommand() {
+  console.log("concert");
+  axios.get("https://rest.bandsintown.com/artists/" + Command + "/events?app_id=codingbootcamp").then(function(response){
+  console.log(response);
+}
+
+// function spotifyCommand() {
+//   console.log("spotify");
+//   axios.get("https://rest.bandsintown.com/artists/" + Command + "/events?app_id=codingbootcamp").then(function(response){
+//   console.log(response);
+// }
+
+// function movieCommand() {
+//   console.log("movie");
+//   axios.get("https://rest.bandsintown.com/artists/" + Command + "/events?app_id=codingbootcamp").then(function(response){
+//   console.log(response);
+// }
+
+// function sayCommand() {
+//   console.log("say");
+//   axios.get("https://rest.bandsintown.com/artists/" + Command + "/events?app_id=codingbootcamp").then(function(response){
+//   console.log(response);
+// }
+
+
+  // console.log("The name of venue: ")
+  // console.log("Venue Location: ")
+  // console.log("Dat of the Event: ")
+
+})
