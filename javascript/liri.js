1a41bff5f4f726507d22b46de03a4950297df23a
DoDatVu <datvu22@gmail.com>
2019-03-26 19:05:15 -0500

    updated

--- javascript/liri.js
+++ javascript/liri.js
@@ -2,3 +2,22 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+switch (action) {
+  case "concert-this":
+    total();
+    break;
+  
+  case "dspotify-this-song":
+    deposit();
+    break;
+  
+  case "movie-this":
+    withdraw();
+    break;
+  
+  case "do-what-it-says":
+    lotto();
+    break;
+  }
+  
